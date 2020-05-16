0d103720cf04ddfbd0b2ce4ae4d95c02e53912ce
sharifhimu <sharifsorotcu@gmail.com>
2020-05-16 12:34:27 +0600

    Updates

---
+++ precache-manifest.872ef3bc4bbb3140cdefb31085c7cfa3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f4865fd20eb27db01ccd465265d7e3",
+    "url": "/Robofriend/index.html"
+  },
+  {
+    "revision": "5d3201b5bd5a50fb4be4",
+    "url": "/Robofriend/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "fe7eea005abbed9b85cb",
+    "url": "/Robofriend/static/css/main.4609f2f6.chunk.css"
+  },
+  {
+    "revision": "5d3201b5bd5a50fb4be4",
+    "url": "/Robofriend/static/js/2.e61a5245.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Robofriend/static/js/2.e61a5245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7eea005abbed9b85cb",
+    "url": "/Robofriend/static/js/main.cf7aa684.chunk.js"
+  },
+  {
+    "revision": "68f88d7dad8099d93fc8",
+    "url": "/Robofriend/static/js/runtime-main.017ce6d0.js"
+  },
+  {
+    "revision": "aaa09bce3fbbe6f74d7ff6586cfa74e9",
+    "url": "/Robofriend/static/media/RobotoMono-Light.aaa09bce.woff"
+  }
+]);
